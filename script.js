8af155deb43c62fcff27cbbafe95a1a76d5788f0
simoneputeo <s.puteo@gmail.com>
2025-04-03 13:22:05 +0200

    fine

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-//1. Definisco la stampa incrementale dei numeri da 1 a 100 usando for
-for (let i = 1; i <= 100; i++)
-//2. Inserisco la prima regola che i numeri divisibili per 15 restituiranno FizzBuzz
-{   if (i % 15 === 0) {
-    console.log("FizzBuzz");}
-//3. Inserisco la seconda regola che i numeri divisibili per 3 restituiranno Fizz
- else if (i % 3 === 0) {
-    console.log("Fizz");}
-//4. Inserisco la terza regola che i numeri divisibili per 5 restituiranno Buzz
-else if (i%5===0){
-    console.log("Buzz");}
-//5. Inserisco la quarta regola che i numeri rimanenti restituiranno il proprio valore
-else {console.log(i);}
- }
+const testoInserito = prompt("Inserisci il testo che vuoi venga convertito in alfabeto farfallino");
+let testoFarfallino = "";
+for (let i = 0; i < testoInserito.length; i++) {
+    const lettera = testoInserito[i];
+    testoFarfallino +=
+            lettera === "a" ||
+            lettera === "e" ||
+            lettera === "i" ||
+            lettera === "o" ||
+            lettera === "u"
+                    ? `${lettera}f${lettera}`
+                    : lettera;
+}
+alert(testoFarfallino);
